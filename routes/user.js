e33239f79258c4962b8061ab7f1114b8ebf7c734
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-10-23 02:47:17 +0600

    add

---
+++ routes/user.js
@@ -0,0 +1,18 @@
+const express =require('express');
+const router =express.Router();
+const db = require('../db');
+
+
+router.post('/save', (req, res) => {
+
+    const { name, email, password, phone, photo } = req.body;
+    const activeStatus_value = true;
+    const userRole = 'CONSUMER';
+
+    const sql = 'insert into user(name,email, password, phone, role, photo, activeStatus) values(?,?,?,?,?,?,?)';
+
+    db.query(sql, [name, email, password, phone, userRole, photo, activeStatus_value], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.send({ message: 'User added successfully!', id: result.insertId });
+    });
+});
