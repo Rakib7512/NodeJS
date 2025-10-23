6c5a87f7f0f6778bb1aacdd27c60bce74d1d32f7
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-10-23 19:04:12 +0600

    add

--- routes/user.js
+++ routes/user.js
@@ -5,13 +5,13 @@ const db = require('../db');
 
 router.post('/save', (req, res) => {
 
-    const { name, email, password, phone, photo } = req.body;
+    const { name, email, password, photo } = req.body;
     const activeStatus_value = true;
     const userRole = 'CONSUMER';
 
-    const sql = 'insert into user(name,email, password, phone, role, photo, activeStatus) values(?,?,?,?,?,?,?)';
+    const sql = 'insert into user(name,email, password, role, photo, activeStatus) values(?,?,?,?,?,?)';
 
-    db.query(sql, [name, email, password, phone, userRole, photo, activeStatus_value], (err, result) => {
+    db.query(sql, [name, email, password, userRole, photo, activeStatus_value], (err, result) => {
         if (err) return res.status(500).send(err);
         res.send({ message: 'User added successfully!', id: result.insertId });
     });
